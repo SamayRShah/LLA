f743e813ea9fd79920eb67caae24431ea59c43ec
SamayRShah <srs6812@rit.edu>
2026-03-12 01:44:35 -0400

    added nullptr guard

--- z2h/kvproject/src/kv.c
+++ z2h/kvproject/src/kv.c
@@ -133,6 +133,7 @@ int kv_delete(kv_t *db, const char*key)
 
 void kv_free(kv_t *db)
 {
+    if(!db) return;
     for(size_t i = 0; i < db->capacity; i++)
     {
         kv_entry_t *e = &db->entries[i];
